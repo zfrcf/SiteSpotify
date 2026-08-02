b4b99c5fbb6b439e02af599d60f1c414a54b799c
zfrcf <chaarlieflow@gmail.com>
2026-08-02 15:11:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+async function doSend() {
+<a class="btn btn-principal" href="mailto:contact@xomad.fr?subject=Découvrir%20H.LA%20DROGUE">Salut, je voulais te parler de ...</a>
+}
+
 $("me-contacter").onclick = doSend;
